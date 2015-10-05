436eaed1ce5343f2e7e95035adec69e320e538f3
Jim McGaw <jpmcgaw@gmail.com>
2015-10-04 20:15:59 -0700

    update wsgi

--- prattle/wsgi.py
+++ prattle/wsgi.py
@@ -7,7 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+import os, site, sys
+
+# From http://jmoiron.net/blog/deploying-django-mod-wsgi-virtualenv/
+
+vepath = '/home/jim/source/prattle/lib/python2.7/site-packages/'
+
+prev_sys_path = list(sys.path)
+
+site.addsitedir(vepath)
+
+sys.path.append('/home/jim/source')
+# sys.path.append('/home/jim/source/prattle')
+sys.path.append('/home/jim/source/prattle/prattle')
+
+# reorder sys.path so new directories from the addsitedir show up first
+new_sys_path = [p for p in sys.path if p not in prev_sys_path]
+for item in new_sys_path:
+    sys.path.remove(item)
+sys.path[:0] = new_sys_path
 
 from django.core.wsgi import get_wsgi_application
 
