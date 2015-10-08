3cea48e680065e604bf0b9156d673b1098d6f2db
Jim McGaw <jpmcgaw@gmail.com>
2015-10-08 02:00:43 -0400

    update wsgi file

--- prattle/wsgi.py
+++ prattle/wsgi.py
@@ -1,36 +1,28 @@
 """
-WSGI config for prattle project.
+WSGI config for prosaicmosaic project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
 """
-
-import os, sys
-
-import os, site, sys
-
-# From http://jmoiron.net/blog/deploying-django-mod-wsgi-virtualenv/
-sys.path.append('/home/jim/source')
-sys.path.append('/home/jim/source/prattle/prattle')
-
-vepath = '/home/jim/source/prattle/lib/python2.7/site-packages/'
-
-prev_sys_path = list(sys.path)
-
-site.addsitedir(vepath)
-
-# reorder sys.path so new directories from the addsitedir show up first
-new_sys_path = [p for p in sys.path if p not in prev_sys_path]
-for item in new_sys_path:
-    sys.path.remove(item)
-sys.path[:0] = new_sys_path
-
-print sys.path
-
-from django.core.wsgi import get_wsgi_application
+import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prattle.settings")
 
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
